e2dc6ad0693fde76c43c829a6b764cce36b10c53
David <davidadriansantoslopez@gmail.com>
2023-03-18 23:29:41 -0300

    First commit

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+
+</head>
+<body>
+    <h3>Nimble Storage System</h3>
+    <div style="color:brown">
+        <h2>Concepto</h2>
+        <p>
+            Una aplicacion web para la gestion de un deposito mayorista.
+
+            Funciones aspiradas:
+            <ol>
+                <li>Control de espacios disponibles</li>
+                <li>Registro de cada articulo por codigo, stock</li>
+                <li>Busqueda de articulos</li>
+                <li>Usuarios con distintos permisos: Peon, Admin, Master</li>
+                <li>Potencial escalable de la app: posibilidad de disennar
+                    mapa de estanterias por parte del usuario, ABML.
+                </li>
+                <li>Gestion de pedidos de mercaderia, asociando clientes
+                    con pedidos de mercaderia. Al hacer el pedido la mercaderia
+                    deberia descontarse del stock. El admin no tendria que dar de baja
+                    articuos de forma manual, aunque la funcion exista para exepciones.
+                </li>
+                <li>Control de envios de pedidos. Cronograma semanal. Rutas. Demora estimada. Ubicacion de unidades. Chat chofer-admins</li>
+                
+
+            </ol>
+        </p>
+    </div>
+</body>
+</html>
